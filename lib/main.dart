a44af3c613005a454ea6b0ba545c9ad4d60c8fa4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-23 06:57:42 +0100

    A splash screen is created

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Role Based Auth",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Role Based App",
+                style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+              ),
+            ]
+          ),
+        ),
       ),
     );
   }
