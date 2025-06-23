d4d12ef72fad157fc480187df000ba8b022fd990
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-23 15:49:14 +0100

    debugShowCheckedModeBanner is set to true again

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: "Role Based Auth",
       theme: ThemeData(
         primarySwatch: Colors.blue,
