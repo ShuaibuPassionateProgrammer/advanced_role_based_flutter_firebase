39bf99c8acfa4026b05996528ede2645376efe05
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-23 07:08:01 +0100

    The abstract inherited method initState is added

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
