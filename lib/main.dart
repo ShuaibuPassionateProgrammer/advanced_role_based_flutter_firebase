d7fdc40beffb0b7a9baa7796566260c7bf5ce989
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-23 07:18:14 +0100

    LoginScreen is created

--- lib/main.dart
+++ lib/main.dart
@@ -56,3 +56,31 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Screen"),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Login to your Account")
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
