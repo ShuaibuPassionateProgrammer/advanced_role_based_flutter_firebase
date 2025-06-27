0138a5e32933ec859d36a5a2f2d9d282bcf16cdc
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-27 11:45:48 +0100

    firebase_options.dart file is imported in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:firebase_core/firebase_core.dart";
+import "package:role_based_auth/firebase_options.dart";
 
 void main() {
   FlutterWidgetsBinding.ensureInitialized();
