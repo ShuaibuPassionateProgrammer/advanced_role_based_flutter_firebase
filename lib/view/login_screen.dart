4655e92c6c45ea12ca81ff726370ac903f7e7d39
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-27 12:00:02 +0100

    All the bug is for setting up the StatefulWidget ins LoginScreen class

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -4,10 +4,10 @@ class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScrenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends StatefulWidget {
+class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
