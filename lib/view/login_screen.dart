9d093a08d735c6099c98384b505918877131ab5b
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-27 11:56:32 +0100

    Done setting up the StatefulWidget in LoginScreen class

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -2,4 +2,15 @@ import "package:flutter/material.dart";
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScrenState();
+}
+
+class _LoginScreenState extends StatefulWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    //
+  }
 }
