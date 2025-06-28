81066e9bea0b52525c60d1d6450e07ce5b680bef
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-28 07:03:25 +0100

    signup method inside the AuthService class is created

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,4 +4,6 @@ import "package:firebase_auth/firebase_auth.dart";
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String?> signup ({ required String name, required String email, required String password, required String role }) {}
 }
