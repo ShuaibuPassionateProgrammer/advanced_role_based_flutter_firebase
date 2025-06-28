ba9d6d4c677838f46506a806b9f456a1d47a8c5d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-28 07:01:04 +0100

    FirebaseFirestore instance is generated

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,4 +3,5 @@ import "package:firebase_auth/firebase_auth.dart";
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 }
